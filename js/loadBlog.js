c7f053e606a9189a810caf4b58c5233a51081005
qrcc <qren@qq.com>
2020-01-02 16:53:58 +0800

    2

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -4,16 +4,14 @@ function loadBlog(date) {
     xmltext.send();
     a = xmltext.responseXML;
     var div = document.getElementById("blog");
-    alert(1);
     //document.getElementById("xmlid").innerHTML = a.getElementsByTagName("to")[2].childNodes[0].nodeValue;
     x = a.getElementsByTagName("date");
-    alert(x)
     var sectionLaber = document.createElement("section");
     sectionLaber.className = "dt-main effect";
     div.appendChild(sectionLaber);
     for(i=0;i<x.length;i++)
     {
-        
-        alert("ssssss");
+        var date = x[i].getElementsByTagName("dateLine")[0].nodeValue
+        alert( x[i].getElementsByTagName("dateLine")[0].nodeValue);
     }
 }
