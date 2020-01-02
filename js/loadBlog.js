1cde7173f9d0240168b9318bc399810d4b997a45
qrcc <qren@qq.com>
2020-01-02 17:45:19 +0800

    Update loadBlog.js

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -7,18 +7,13 @@ function loadBlog(date) {
     //document.getElementById("xmlid").innerHTML = a.getElementsByTagName("to")[2].childNodes[0].nodeValue;
     x = a.getElementsByTagName("date");
     div.appendChild(sectionLaber);
+    alert("2222");
     for(i=0;i<x.length;i++)
     {
         var sectionLaber = document.createElement("section");
         alert("1111");
         sectionLaber.className = "dt-main effect";
-        alert(0);
-        var divContentLaber = document.createElement("div");
-        alert(1);
-        divContentLaber.className = "dt-content";
-        alert(2);
-        sectionLaber.appendChild(divContentLaber);
-        alert(3);
+        
         div.appendChild(sectionLaber);
         alert(4);
         var date = x[i].getElementsByTagName("dateLine")[0].childNodes[0].nodeValue;
