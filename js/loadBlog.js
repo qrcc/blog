f90907d77f585a850c73f5b04ed068899410c766
qrcc <qren@qq.com>
2020-01-02 17:47:50 +0800

    Update loadBlog.js

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -6,16 +6,11 @@ function loadBlog(date) {
     var div = document.getElementById("blog");
     //document.getElementById("xmlid").innerHTML = a.getElementsByTagName("to")[2].childNodes[0].nodeValue;
     x = a.getElementsByTagName("date");
-    div.appendChild(sectionLaber);
-    alert("2222");
     for(i=0;i<x.length;i++)
     {
         var sectionLaber = document.createElement("section");
-        alert("1111");
         sectionLaber.className = "dt-main effect";
-        
         div.appendChild(sectionLaber);
-        alert(4);
         var date = x[i].getElementsByTagName("dateLine")[0].childNodes[0].nodeValue;
     }
 }
