e322a5a44ee88be465411836a3205b2a8856fcf6
qrcc <qren@qq.com>
2020-01-02 14:46:15 +0800

    Update loadBlog.js

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -3,11 +3,14 @@ function loadBlog(date) {
     xmltext.open("GET","../xml/blog.xml",false);
     xmltext.send();
     a = xmltext.responseXML;
-    var div = getElementById("xmlid")
+    var div = getElementById("blog")
     //document.getElementById("xmlid").innerHTML = a.getElementsByTagName("to")[2].childNodes[0].nodeValue;
-    x = a.getElementsByTagName("date");
+    x = a.getElementsByTagName("blog");
     for(i=0;i<x.length;i++)
     {
+        var sectionLaber = document.createElement("section")
+        section.className = "dt-main effect"
+        div.appendChild(sectionLaber);
         document.write("<div class='aaaa'>");
         document.write(x[i].getElementsByTagName("to")[0].childNodes[0].nodeValue);
         document.write("</div>");
