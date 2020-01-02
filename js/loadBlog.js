00e881ef2e320cd8dbe371100994d2df110a0da1
qrcc <qren@qq.com>
2020-01-02 15:26:56 +0800

    Update loadBlog.js

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -4,7 +4,7 @@ function loadBlog(date) {
     xmltext.send();
     a = xmltext.responseXML;
     var div = document.getElementById("blog");
-    alert("asdasdas");
+    alert(div);
     //document.getElementById("xmlid").innerHTML = a.getElementsByTagName("to")[2].childNodes[0].nodeValue;
     x = a.getElementsByTagName("blog");
     var sectionLaber = document.createElement("p");
