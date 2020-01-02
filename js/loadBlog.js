96f83992dab77bc93487501ec9457e9a353641e1
qrcc <qren@qq.com>
2020-01-02 15:21:45 +0800

    Update loadBlog.js

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -3,17 +3,16 @@ function loadBlog(date) {
     xmltext.open("GET","../xml/blog.xml",false);
     xmltext.send();
     a = xmltext.responseXML;
-    var div = document.getElementById("blog")
-    alert("asdasdas")
+    var div = document.getElementById("blog");
+    alert("asdasdas");
     //document.getElementById("xmlid").innerHTML = a.getElementsByTagName("to")[2].childNodes[0].nodeValue;
     x = a.getElementsByTagName("blog");
-    var sectionLaber = document.createElement("section")
-    section.className = "dt-main effect111"
-    div.appendChild(sectionLaber);
+    var sectionLaber = document.createElement("p");
+    section.className = "dt-main effect111";
     div.appendChild(p);
     for(i=0;i<x.length;i++)
     {
         
-        alert("ssssss")
+        alert("ssssss");
     }
 }
