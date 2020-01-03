b86b3f598025e461e08f6c1c30bc204121777720
qrcc <qren@qq.com>
2020-01-03 10:15:12 +0800

    Update loadBlog.js

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -6,13 +6,19 @@ function loadBlog(date) {
     var div = document.getElementById("blog");
     //document.getElementById("xmlid").innerHTML = a.getElementsByTagName("to")[2].childNodes[0].nodeValue;
     x = a.getElementsByTagName("date");
-    for(i=0;i<x.length;i++)
+    for(i = 0; i < x.length; i++)
     {
         var dateLine = x[i].getElementsByTagName("dateLine")[0].childNodes[0].nodeValue;
         var headLine = x[i].getElementsByTagName("headLine")[0].childNodes[0].nodeValue;
         var bodyLine = x[i].getElementsByTagName("bodyLine")[0].getElementsByTagName("section")[0].childNodes[0].nodeValue;
         var inner = "<div>"+dateLine+"</div>";
         inner = inner + "<h1>"+headLine+"</h1>";
+        var sectionLines = x[i].getElementsByTagName("bodyLine")[0].getElementsByTagName("section");
+        for(j = 0; j < sectionLines.length; j++)
+        {
+            alert(sectionLines[j].childNodes[0].nodeValue);
+            inner = inner + "<p>"+ sectionLines[j].childNodes[0].nodeValue +"</p>";
+        }
         alert(inner);
         var sectionLaber = document.createElement("section");
         sectionLaber.className = "dt-main effect";
