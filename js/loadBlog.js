4881118282ebf2bd4e1a1cd8fe5345aeecddf193
qrcc <qren@qq.com>
2020-01-05 18:35:05 +0800

    2

--- js/loadBlog.js
+++ js/loadBlog.js
@@ -62,6 +62,7 @@ function loadBlog(date) {
                     inner = inner + "<img src=\"../blogImg/" + routeDay + "/" + imgName + "\">";
                     //图片描述
                     imgDesc = images[k].getAttribute("describe");
+                    alert(imgDesc);
                     if(imgDesc != "null"){ inner = inner + "<p class = \"imgDesc\">" + imgDesc + "</p>";}
                 }
             }
