4c8c48366b29eea7c2fc2ffcad25dc526d71b172
qrcc <qren@qq.com>
2020-02-02 22:15:48 +0800

    1

--- js/loadTech.js
+++ js/loadTech.js
@@ -17,7 +17,13 @@ function loadBlog(id) {
     //获得参数：每页列出的文档数,文档所属日期,以此获得对应XML文件的位置
     var listNum = 2;
     var routeDay = GetQueryString("d");
-    var route = "../xml/" + id + "/blog"+ routeDay +".xml";
+    if(id == "blog"){
+        var route = "../xml/" + id + "/blog"+ routeDay +".xml";
+    }
+    else if(id == "tech"){
+        var route = "../xml/" + id + "/blog"+ routeDay +".xml";
+    }
+    
     alert(route);
     var pageNum = GetQueryString("page");
     //获得XML文件
