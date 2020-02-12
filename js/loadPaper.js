214cfc07f24ae7629af5dfb7a5c238663c3e0173
qrcc <qren@qq.com>
2020-02-12 14:49:11 +0800

    1

--- js/loadPaper.js
+++ js/loadPaper.js
@@ -41,7 +41,6 @@ function loadBlog(id) {
     {
         var dateLine = x[i].getElementsByTagName("dateLine")[0].childNodes[0].nodeValue;
         var date = x[i].getAttribute("name");
-        alert(date);
         var headLine = x[i].getElementsByTagName("headLine")[0].childNodes[0].nodeValue;
         var bodyLine = x[i].getElementsByTagName("bodyLine")[0].getElementsByTagName("section")[0].childNodes[0].nodeValue;
         var inner = "<div class = \"article_tit_txt\">";
